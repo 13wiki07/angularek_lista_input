128fd5daeaba56a93cc87f747240cd9fb5e9f9e3
13wiki07 <wikzel13@gmail.com>
2022-09-16 08:26:16 +0200

    new class miejsca, add images

---
+++ src/app/miejsca.ts
@@ -0,0 +1,9 @@
+import { Miejsce } from "./miejsce";
+
+export class Miejsca {
+    public miejsca: Miejsce [] = [
+        new Miejsce("Paris", 1, "assets/images/paris.png"),
+        new Miejsce("Berlin", 10, "assets/images/berlin.png"),
+        new Miejsce("London", 5, "assets/images/london.png")
+    ]
+}
