3afc72082b9cbcb19bcae796b73346fe10bf81eb
13wiki07 <wikzel13@gmail.com>
2022-09-16 11:31:29 +0200

    READY

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Miejsca } from './miejsca';
 import { Miejsce } from './miejsce';
 
@@ -13,6 +14,11 @@ export class AppComponent {
   miejscaWyswietlane: Miejsce [] = Miejsca.miejsca;
   indeksMiejsca: number = 0;
   wybraneMiejsce: Miejsce = this.miejscaWyswietlane[0];
+
+  onSubmit(f: NgForm): void { // dodaj import NgForms
+    console.log(f.value);
+    console.log(this.indeksMiejsca);
+  }
   // elementy: string[] = [
   //   "London",
   //   "Berlin",
